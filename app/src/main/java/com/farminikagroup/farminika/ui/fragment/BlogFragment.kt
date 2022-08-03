98fc87bbf752ba111c67729cd55621d110376f68
keithchad <keithchad10@outlook.com>
2022-08-03 21:55:45 +0300

    blog user interface

--- app/src/main/java/com/farminikagroup/farminika/ui/fragment/BlogFragment.kt
+++ app/src/main/java/com/farminikagroup/farminika/ui/fragment/BlogFragment.kt
@@ -5,16 +5,98 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.farminikagroup.farminika.R
+import com.farminikagroup.farminika.data.adapter.BlogAdapter
+import com.farminikagroup.farminika.data.model.Blog
+import kotlinx.android.synthetic.main.fragment_blog.*
 
 class BlogFragment : Fragment() {
-
+    lateinit var blog: List<Blog>
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        populateData()
+        initializeRecyclerView()
         return inflater.inflate(R.layout.fragment_blog, container, false)
+    }
+
+    private fun initializeRecyclerView() {
+        val blogAdapter = BlogAdapter(list, context)
+        recyclerView.layoutManager = LinearLayoutManager
+        recyclerView.adapter = blogAdapter
+    }
+
+    private fun populateData() {
+         blog = listOf(
+            Blog(
+                1,
+                R.drawable.dummy_photo_1,
+                "Welcome to Kenya for the best experience!",
+                R.drawable.dummy_photo,
+                "Keith Chad",
+                "20 Minutes"
+            ),
+            Blog(
+                1,
+                R.drawable.dummy_photo_1,
+                "Welcome to Kenya for the best experience!",
+                R.drawable.dummy_photo,
+                "Keith Chad",
+                "20 Minutes"
+            ),
+            Blog(
+                1,
+                R.drawable.dummy_photo_1,
+                "Welcome to Kenya for the best experience!",
+                R.drawable.dummy_photo,
+                "Keith Chad",
+                "20 Minutes"
+            ),
+            Blog(
+                1,
+                R.drawable.dummy_photo_1,
+                "Welcome to Kenya for the best experience!",
+                R.drawable.dummy_photo,
+                "Keith Chad",
+                "20 Minutes"
+            ),
+            Blog(
+                1,
+                R.drawable.dummy_photo_1,
+                "Welcome to Kenya for the best experience!",
+                R.drawable.dummy_photo,
+                "Keith Chad",
+                "20 Minutes"
+            ),
+            Blog(
+                1,
+                R.drawable.dummy_photo_1,
+                "Welcome to Kenya for the best experience!",
+                R.drawable.dummy_photo,
+                "Keith Chad",
+                "20 Minutes"
+            ),
+            Blog(
+                1,
+                R.drawable.dummy_photo_1,
+                "Welcome to Kenya for the best experience!",
+                R.drawable.dummy_photo,
+                "Keith Chad",
+                "20 Minutes"
+            ),
+            Blog(
+                1,
+                R.drawable.dummy_photo_1,
+                "Welcome to Kenya for the best experience!",
+                R.drawable.dummy_photo,
+                "Keith Chad",
+                "20 Minutes"
+            )
+
+        )
     }
 
 }
